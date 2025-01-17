432d8c6deaf33228c7d98eda31e030c6951d33aa
Anshu kumar <anshu4125kumar@gmail.com>
2025-01-18 01:21:15 +0530

    added postOffice card

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let IP_Address;
-let userData;
+let postal;
+let timeZone;
 
 document.addEventListener("DOMContentLoaded", () => {
   fetch("https://ipinfo.io/json")
@@ -10,20 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
             <h2>Your Current IP Address is <span>${data.ip}</span></h2>
             <button id="start-btn" onclick="fetchUserInfo()">Get Started</button>
         `;
-      console.log(`IP Address: ${data.ip}`);
     })
     .catch((error) => {
       console.error("Error fetching IP address:", error);
     });
 });
+
 const fetchUserInfo = () => {
   document.getElementById("hero").style.display = "none";
   document.getElementById("data").style.display = "block";
   fetch(`https://ipapi.co/${IP_Address}/json/`)
     .then((response) => response.json())
     .then((data) => {
-      userData = data;
-      console.log(userData);
+      postal = data.postal;
+      timeZone = data.timezone;
       document.getElementById("data-container").innerHTML = `
          <p>IP Address: <span>${IP_Address}</span></p>
             <div class="grid-row">
@@ -50,13 +51,34 @@ const fetchUserInfo = () => {
       document.getElementById("map-item").innerHTML = `
         <iframe src="https://maps.google.com/maps?q=${data.latitude},${data.longitude}&z=15&output=embed" width="100%" height="100%" frameborder="0" style="border:0"></iframe>
         `;
+      fetchPostOffice();
+    });
+};
+
+const fetchPostOffice = () => {
+  fetch(`https://api.postalpincode.in/pincode/${postal}`)
+    .then((res) => res.json())
+    .then((data) => {
       document.getElementById("userInfo").innerHTML = `
-            <p>Time Zone: <span>${data.timezone}</span></p>
+            <p>Time Zone: <span>${timeZone}</span></p>
             <p>Date And Time: <span>${new Date().toLocaleString("en-US", {
-              timeZone: data.timeZone,
+              timeZone: timeZone,
             })}</span></p>
-            <p>Pincode: <span>${data.postal}</span></p>
-            <p>Message: Number of pincode(s) found: <span>01</span></p>
+            <p>Pincode: <span>${postal}</span></p>
+            <p>Message: <span>${data[0].Message}</span></p>
         `;
+      let post_container = document.getElementById("post-container");
+      data[0].PostOffice.map((postOffice) => {
+        post_container.innerHTML += `
+        
+         <div class="post"> 
+            <p>Name: <span>${postOffice.Name}</span></p>
+            <p>Branch Type: <span>${postOffice.BranchType}</span></p>
+            <p>Delivery Status: <span>${postOffice.DeliveryStatus}</span></p>
+            <p>District: <span>${postOffice.District}</span></p>
+            <p>Division: <span>${postOffice.Division}</span></p>
+        </div>
+        `;
+      });
     });
 };
